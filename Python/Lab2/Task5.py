7851460105ba68250ac679bce420bdff88da0129
Oronemu <Oronemu@mail.ru>
2022-03-01 22:04:03 +0700

    Little changes
    bebra bebra

--- Python/Lab2/Task5.py
+++ Python/Lab2/Task5.py
@@ -1,6 +1,6 @@
 import random
 
-list = [random.randint(0, 9) for i in range(0, 10)]
+list = [random.randint(0, 10) for i in range(0, 10)]
 print(list)
 
 for i in range(1, len(list)):
--- Python/Lab2/Task6.py
+++ Python/Lab2/Task6.py
@@ -1,6 +1,6 @@
 import random
 
-list = [random.randint(-9, 9) for i in range(0, 10)]
+list = [random.randint(-10, 10) for i in range(0, 10)]
 print(list)
 
 for i in range(1, len(list)):
