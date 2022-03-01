0e5313973f7f71ba4f8beb781c436da808a6530c
Oronemu <Oronemu@mail.ru>
2022-03-01 23:35:11 +0700

    Little changes

--- Python/Lab2/Task9.py
+++ Python/Lab2/Task9.py
@@ -1,19 +1,19 @@
 count_of_queens = 8
-width = []
-height = []
-is_correct = True
+x = []
+y = []
+correct = True
 
-for i in range(count_of_queens):
-    width_input, height_input = [int(line) for line in input().split()]
-    width.append(width_input)
-    height.append(height_input)
+for i in range(0, count_of_queens):
+    x_input, y_input = [int(line) for line in input().split()]
+    x.append(x_input)
+    y.append(y_input)
 
 for i in range(count_of_queens):
     for j in range(i + 1, count_of_queens):
-        if width[i] == width[j] or height[i] == height[j] or abs(width[i] - width[j]) == abs(height[i] - height[j]):
+        if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
             correct = False
 
-if is_correct:
+if correct:
     print('NO')
 else:
     print('YES')
